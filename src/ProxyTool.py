3afd1f85586ee25889979097e8f5e77ae04766d9
Lee7goal <36347719+Lee7goal@users.noreply.github.com>
2022-11-15 10:54:34 +0800

    Update ProxyTool.py

--- src/ProxyTool.py
+++ src/ProxyTool.py
@@ -1,4 +1,5 @@
 import requests
+import time
 
 
 class ProxyTools:
@@ -7,6 +8,14 @@ class ProxyTools:
         self.session.headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.42'
         }
+
+    def check(self, proxies):
+        url = 'https://www.163.com'
+        resp = self.session.get(url, proxies=proxies, timeout=20)
+        if resp.status_code == 200:
+            return True
+        else:
+            return False
 
     def get(self):
         resp = self.session.get('http://demo.spiderpy.cn/get/')
@@ -19,3 +28,11 @@ class ProxyTools:
             return proxies
         else:
             return None
+
+    def run(self):
+        while True:
+            p = self.get()
+            status = self.check(p)
+            if status is True:
+                return p
+            time.sleep(5)
